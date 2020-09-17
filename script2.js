705c2bc3625c7c92caea4aec3b4c2416038c50bd
Alexey <malykhinal@mail.ru>
2020-09-17 15:32:11 +0500

    +
    ветвь для PULrequest

---
+++ script2.js
@@ -0,0 +1,5 @@
+var name = "Василий";
+var admin = name;
+document.write("<h1> Задание №2</h1>")
+document.write("Переменная admin равна " + admin + "<br>");
+document.write("Переменная name равна " + name);
