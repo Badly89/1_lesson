f4cba954cf0ba80f3a18da3f821395fbb2ad0d9e
Alexey <malykhinal@mail.ru>
2020-09-17 15:32:11 +0500

    +

---
+++ script1.js
@@ -0,0 +1,4 @@
+var TempC = parseInt(prompt("Укажите значение температуры в цельсиях:"));
+var TempF = (9 / 5) * TempC + 32;
+document.write("<h1> Задание №1</h1>")
+document.write("Температура по Фарингейту равна: " + TempF + " &#8457;");
